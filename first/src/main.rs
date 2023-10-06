7b0888a42f0867a61edbd9884bb5831c8c059ae3
Jaroslav Langer <langer.jaros@gmail.com>
2023-10-06 19:13:33 +0200

    first: Hello world translations were added.

--- first/src/main.rs
+++ first/src/main.rs
@@ -1,3 +1,20 @@
+//! # First 🦀 Project
+
+/// Pairs of "hello world" translations.
+/// First element is an alpha-3/ISO 639-2 T (terminology) code of the language.
+/// Second element is a google translation of "hello world" into the language.
+///
+/// Languages were selected based on the countries
+/// mentioned in the "lets-meet" discord room.
+static HELLO_WORLDS: [(&str, &str); 5] = [
+    ("ces", "ahoj světe"),
+    ("slk", "ahoj svet"),
+    ("ukr", "привіт світ"),
+    ("ara", "مرحبا بالعالم"),
+    ("ben", "ওহে বিশ্ব"),
+];
+
+/// Greets the world in many human languages.
 fn main() {
-    println!("Hello, world!");
+    println!("{:#?}", HELLO_WORLDS)
 }
