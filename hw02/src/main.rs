b8accf0b259c84b0baf8e3cdc1cac208ec4c8c56
Jaroslav Langer <langer.jaros@gmail.com>
2023-10-16 23:44:35 +0200

    hw02: Changes towards more functional approach. Support for a new argument "one-space".

--- hw02/src/main.rs
+++ hw02/src/main.rs
@@ -1,49 +1,50 @@
 //! [Text Transformation](https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework)
 
+use regex::Regex;
 use std::env;
+use std::error::Error;
 use std::io;
 use std::io::Read; // Needed for Stdin.read_to_string
-use std::process;
 
-fn main() {
-    let transform = choose_transformation();
-    let text = read_input();
-    print!("{}", transform(text.as_str()));
+fn main() -> Result<(), Box<dyn Error>> {
+    let transform = choose_transformation()?;
+    let text = read_input()?;
+    Ok(print!("{}", transform(text.as_str())))
 }
 
 /// Choose transformation function based on command argument given.
-fn choose_transformation() -> fn(&str) -> String {
-    match read_argument().as_str() {
+fn choose_transformation() -> Result<fn(&str) -> String, String> {
+    Ok(match read_single_argument()?.as_str() {
         "lowercase" => str::to_lowercase,
         "uppercase" => str::to_uppercase,
         "no-spaces" => |s: &str| -> String { s.replace(' ', "") },
         // "slugify" => slug::slugify, // Does not work :/
         "slugify" => |s: &str| -> String { slug::slugify(s) },
-        _ => panic!("Error: unsupported argument given!"),
-    }
+        "one-space" => {
+            |s: &str| -> String { Regex::new(r"\s+").unwrap().replace_all(s, " ").to_string() }
+        }
+        other => return Err(format!("Argument \"{}\" is not supported!", other)),
+    })
 }
 
-fn read_argument() -> String {
+/// Returns the command line argument if only one given,
+/// otherwise returns an error.
+fn read_single_argument() -> Result<String, String> {
     let mut args = env::args().skip(1);
-    let Some(arg) = args.next() else {
-        error("Error: missing required argument!");
-    };
-    if args.len() > 0 {
-        error("Error: more than one argument given!");
+    if let Some(arg) = args.next() {
+        if args.len() == 0 {
+            Ok(arg)
+        } else {
+            Err(String::from("More than one argument given!"))
+        }
+    } else {
+        Err(String::from("Missing required argument!"))
     }
-    arg
-}
-
-fn error(msg: &str) -> ! {
-    eprintln!("{}", msg);
-    process::exit(1);
 }
 
 /// Read a string from standard input.
-fn read_input() -> String {
-    let mut s = String::new();
-    io::stdin()
-        .read_to_string(&mut s)
-        .expect("standard input should be readable");
-    s
+fn read_input() -> io::Result<String> {
+    let mut text = String::new();
+    io::stdin().read_to_string(&mut text)?;
+    Ok(text)
 }
