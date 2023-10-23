a8ac3fe96060b381709dddbcab3037cda2d79836
Jaroslav Langer <langer.jaros@gmail.com>
2023-10-23 14:41:28 +0200

    1. Refactor main() Function: main function satisfies the requirements, documentation was improved.

--- text-tool/src/main.rs
+++ text-tool/src/main.rs
@@ -1,4 +1,8 @@
-//! [Text Transformation](https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework)
+//! # Text Transformation Tool
+//!
+//! Implements requirements from
+//! <https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework> and
+//! <https://robot-dreams-rust.mag.wiki/5-error-handling/index.html#homework>.
 
 use regex::Regex;
 use std::env;
@@ -6,10 +10,23 @@ use std::error::Error;
 use std::io;
 use std::io::Read; // Needed for Stdin.read_to_string
 
+/// When there are no errors it reads the standard input, transforms it
+/// and prints the result to the standard output.
+///
+/// The transformation is chosen based on command argument.
+///
+/// In case of an error, there is an attempt to print the error to the standard
+/// error and the process returns `ExitCode::FAILURE`
+/// (see <https://doc.rust-lang.org/src/std/process.rs.html#2291-2301>).
+///
+/// It looks like both `eprintln!` macro and `io::attempt_print_to_stderr` uses
+/// `stderr().write_fmt(args)` in the end
+/// (see <https://doc.rust-lang.org/src/std/io/stdio.rs.html#1039>).
 fn main() -> Result<(), Box<dyn Error>> {
     let transform = choose_transformation()?;
     let text = read_input()?;
-    Ok(print!("{}", transform(text.as_str())))
+    print!("{}", transform(text.as_str()));
+    Ok(())
 }
 
 /// Choose transformation function based on command argument given.
