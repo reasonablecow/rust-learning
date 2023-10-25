ea326649fc6a059f71d8eae45d887f50265a2285
Jaroslav Langer <langer.jaros@gmail.com>
2023-10-25 15:25:04 +0200

    4. Implement the CSV Operation: csv function first iteration.

--- text-tool/src/main.rs
+++ text-tool/src/main.rs
@@ -3,12 +3,13 @@
 //! Implements requirements from
 //! <https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework> and
 //! <https://robot-dreams-rust.mag.wiki/5-error-handling/index.html#homework>.
+//!
+//! Try this out `<example.csv cargo run csv`.
 
 use regex::Regex;
 use std::env;
 use std::error::Error;
 use std::io;
-use std::io::Read;
 
 /// When there are no errors it reads the standard input, transforms it
 /// and prints the result to the standard output.
@@ -24,22 +25,22 @@ use std::io::Read;
 /// (see <https://doc.rust-lang.org/src/std/io/stdio.rs.html#1039>).
 fn main() -> Result<(), Box<dyn Error>> {
     let transform = choose_transformation()?;
-    let text = read_input()?;
-    print!("{}", transform(text.as_str()));
+    let text = io::read_to_string(io::stdin())?;
+    print!("{}", transform(&text)?);
     Ok(())
 }
 
+type ResultStringOrError = Result<String, Box<dyn Error>>;
+
 /// Choose transformation function based on command argument given.
-fn choose_transformation() -> Result<fn(&str) -> String, String> {
+fn choose_transformation() -> Result<fn(&str) -> ResultStringOrError, String> {
     Ok(match read_single_argument()?.as_str() {
-        "lowercase" => str::to_lowercase,
-        "uppercase" => str::to_uppercase,
-        "no-spaces" => |s: &str| -> String { s.replace(' ', "") },
-        // "slugify" => slug::slugify, // Does not work :/
-        "slugify" => |s: &str| -> String { slug::slugify(s) },
-        "one-space" => {
-            |s: &str| -> String { Regex::new(r"\s+").unwrap().replace_all(s, " ").to_string() }
-        }
+        "lowercase" => |s| Ok(s.to_lowercase()),
+        "uppercase" => |s| Ok(s.to_uppercase()),
+        "no-spaces" => |s| Ok(s.replace(' ', "")),
+        "slugify" => |s| Ok(slug::slugify(s)),
+        "one-space" => |s| Ok(Regex::new(r"\s+").map(|p| p.replace_all(s, " ").to_string())?),
+        "csv" => csv,
         other => return Err(format!("Argument \"{}\" is not supported!", other)),
     })
 }
@@ -59,9 +60,42 @@ fn read_single_argument() -> Result<String, String> {
     }
 }
 
-/// Read a string from standard input.
-fn read_input() -> Result<String, Box<dyn Error>> {
-    let mut text = String::new();
-    io::stdin().read_to_string(&mut text)?;
-    Ok(text)
+/// Returns well formatted table string from parsed CSV.
+///
+/// **Warning!**: Line endings are always LF byte.
+/// TODO: If CRLF is desired use argument end-crlf.
+fn csv(s: &str) -> ResultStringOrError {
+    // Split string into rows and rows into fields.
+    let csv = s
+        .lines()
+        .map(|line| line.split(',').map(|s| s.trim()).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    // Check all rows have the same number of fields.
+    let mut row_lengths = csv.iter().map(|h| h.len());
+    let Some(hdr_len) = row_lengths.next() else {
+        return Err("CSV must have an header.".into());
+    };
+    let true = row_lengths.all(|l| l == hdr_len) else {
+        return Err("Every CSV row must have the same number of fields as the header.".into());
+    };
+
+    // Format csv into table string.
+    // TODO: Change longest to vector of lengths for each column.
+    let Some(longest) = csv.iter().flatten().map(|w| w.len()).max() else {
+        return Err("CSV must have at least one row and one column.".into());
+    };
+    Ok(csv
+        .iter()
+        .map(|row| {
+            format!(
+                "|{}|\n",
+                row.iter()
+                    .map(|field| format!(" {:width$} ", field, width = longest))
+                    .collect::<Vec<_>>()
+                    .join("|")
+            )
+        })
+        .collect::<Vec<_>>()
+        .concat())
 }
