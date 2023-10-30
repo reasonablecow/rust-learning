d1cb694af12630cdc542d4ae735c370564b05cf5
Jaroslav Langer <langer.jaros@gmail.com>
2023-10-30 12:00:26 +0100

    interactive mode with "reader" and "editor" threads

--- text-tool/src/main.rs
+++ text-tool/src/main.rs
@@ -1,21 +1,46 @@
 //! # Text Transformation Tool
 //!
+//! TODO: transformation variants change to enum, implement FromStr trait
+//! TODO: interactive: CSV function change read string from path
+//!
 //! Implements requirements from
 //! * <https://robot-dreams-rust.mag.wiki/7-concurrency-multithreading/index.html#homework>
 //! * <https://robot-dreams-rust.mag.wiki/5-error-handling/index.html#homework>
 //! * <https://robot-dreams-rust.mag.wiki/2-rust-basics/index.html#homework>
 //!
-//! Try this out `<example.csv cargo run csv`.
+//! ```sh
+//! # single transformation example:
+//! <example.csv cargo run csv
+//! # multiple transformations example:
+//! <example.txt cargo run >out.txt; echo "Errors? $?"
+//! ```
 
 use core::fmt;
 use regex::Regex;
 use std::error::Error;
-use std::{env, io};
+use std::sync::mpsc;
+use std::{env, io, thread};
 
-/// When there are no errors it reads the standard input, transforms it
-/// and prints the result to the standard output.
+/// Prints transformed standard input based on its values and arguments given.
 ///
-/// The transformation is chosen based on command argument.
+/// Running the executable with an argument triggers an "one-shot" mode,
+/// "interactive" mode runs otherwise.
+///
+/// ## One-shot Mode (single thread)
+///
+/// The function tries to:
+/// 1. read the standard input,
+/// 2. apply a transformation* to it,
+/// 3. print the result to the standard output.
+///
+/// * It is chosen based on the argument given to the executable.
+///
+/// ## Interactive Mode (multi-threaded)
+///
+/// Parses each line of standard input into transformation and its input.
+/// The rest is similar to one-shot mode.
+///
+/// ## Error Details
 ///
 /// In case of an error, there is an attempt to print the error to the standard
 /// error and the process returns `ExitCode::FAILURE`
@@ -25,29 +50,87 @@ use std::{env, io};
 /// `stderr().write_fmt(args)` in the end
 /// (see <https://doc.rust-lang.org/src/std/io/stdio.rs.html#1039>).
 fn main() -> Result<(), Box<dyn Error>> {
-    // TODO: transformation variants change to enum, implement FromStr trait
-    // TODO: interactive: spawn threads input-parser and string-transformer
-    // TODO: interactive: CSV function change read string from path
     if let Some(argument) = read_single_argument()? {
+        // One thread, one transformation, multi-line transformation input
         let transform = parse_transformation(&argument)?;
         let text = io::read_to_string(io::stdin())?;
         print!("{}", transform(&text)?);
         Ok(())
-    } else { // TODO:
-        //spawn_reader
-        //    send
-        //        parse_input
-        //            read_stdin
-        //spawn_writer
-        //    for transform, text in recv
-        //        print
-        //            transform
-        //                text
-        todo!()
+    } else {
+        // Two threads, many transformations, single-line transformation input
+        let (sender, receiver) = mpsc::channel();
+
+        let reader = thread::spawn(move || {
+            let mut buffer = String::new();
+            let stdin = io::stdin();
+
+            loop {
+                buffer.clear();
+                match stdin.read_line(&mut buffer) {
+                    Err(msg) => break Err(msg.to_string()), // wanted to do `e @ Err(_) => return e`
+                    Ok(0) => break Ok(()),                  // EOF
+                    Ok(_) => {
+                        if let Err(msg) = sender.send(parse_line(&buffer)) {
+                            break Err(msg.to_string());
+                        } else {
+                            continue;
+                        };
+                    }
+                }
+            }
+        });
+
+        let editor = thread::spawn(move || {
+            let mut state = Ok(());
+            let general_error = Err("all errors are in the standard error");
+            for content in receiver {
+                match content {
+                    Err(msg) => {
+                        state = general_error;
+                        eprintln!("{}", msg);
+                    }
+                    Ok((transform, text)) => match transform(&text) {
+                        Err(msg) => {
+                            state = general_error;
+                            eprintln!("{}", msg);
+                        }
+                        Ok(s) => println!("{}", s),
+                    },
+                }
+            }
+            state
+        });
+
+        match (reader.join(), editor.join()) {
+            (Ok(Ok(_)), Ok(Ok(_))) => Ok(()),
+            (read, edit) => Err(format!("{:?} | {:?}", read, edit).into()),
+        }
     }
 }
 
 type FnStrToResult = fn(&str) -> Result<String, Box<dyn Error>>;
+
+/// Parses string into transformation function and an argument string
+///
+/// The line parsing should be equivalent to the following regex:
+/// `^\s*(?<transformation>\w+) (?<argument>.*)\n?$`
+/// (<https://docs.rs/regex/latest/regex/index.html#syntax>).
+fn parse_line(raw: &str) -> Result<(FnStrToResult, String), String> {
+    let without_newline = if let Some(stripped) = raw.strip_suffix('\n') {
+        stripped
+    } else {
+        raw
+    };
+    if let Some((cmd, arg)) = without_newline.trim_start().split_once(' ') {
+        // TODO: if transform == csv -> read from path (arg)
+        Ok((parse_transformation(cmd)?, arg.to_string()))
+    } else {
+        Err(format!(
+            "Couldn't parse into `<command> <input>`, data={:?}",
+            raw
+        ))
+    }
+}
 
 /// Returns transformation function based on given argument.
 fn parse_transformation(argument: &str) -> Result<FnStrToResult, String> {
